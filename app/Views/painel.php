e81b8f11e72e80b483556783c6116f123edf7dfb
Gava <gabrielgavat1empreendimentos.com.br>
2025-06-23 10:10:55 -0300

    refactor:altera nomes no painel

--- app/Views/painel.php
+++ app/Views/painel.php
@@ -13,10 +13,10 @@
 <div class="d-flex">
   <!-- Sidebar -->
   <nav class="bg-dark text-white p-3" style="width: 220px; height: 100vh;">
-    <h5 class="text-white">Menu</h5>
+    <h5 class="text-white">Painel Lateral</h5>
     <ul class="nav flex-column">
       <li class="nav-item">
-        <a class="nav-link text-white" href="/painel">Dashboard</a>
+        <a class="nav-link text-white" href="/painel">Painel Principal</a>
       </li>
       <li class="nav-item">
         <a class="nav-link text-white" href="#">Usuários</a>
